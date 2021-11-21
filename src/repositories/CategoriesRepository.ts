9b48f074aead4d22f8e3c8cb92cb9664daa5593d
João Vitor Pacheco de Freitas <joaovotk@gmail.com>
2021-11-21 20:17:50 -0300

    Aplicando principio SRP e LSP do SOLID

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,18 +1,13 @@
 import { Category } from "../models/Category";
-
-interface ICreateCategoryDTO{
-  name: string,
-  description: string
-}
+import { ICategoryRepository, ICreateCategoryDTO } from "./ICategoriesRepository";
 
 
-class CategoriesRepository {
+class CategoriesRepository implements ICategoryRepository{
   private categories: Category[];
 
   constructor(){
     this.categories = [];
   }
-
 
   create({name, description}: ICreateCategoryDTO): void{
     const category: Category = new Category();
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,17 @@
 import { Router } from 'express';
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 
-const categories = [];
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const categoryalreadyExists = categoriesRepository.findByName(name);
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
 
-  if(categoryalreadyExists){
-    return response.status(400).json({Error: "Category already Exists!!!"});
-  }
-
-  categoriesRepository.create({name, description});
+  createCategoryService.execute({name, description});
 
   return response.status(201).send();
 });
