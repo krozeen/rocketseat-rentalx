438d71af00317ef6de01ff443be84a9d5e83285f
João Vitor Pacheco de Freitas <joaovotk@gmail.com>
2021-11-21 01:14:37 -0300

    Estrutura iniciada

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+
+import express from 'express';
+import cors from 'cors';
+import { categoriesRoutes } from './routes/categories.routes';
+
+import 'reflect-metadata';
+//import './database/connection';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(categoriesRoutes);
+
+
+const port = process.env.PORT || 3333;
+
+app.listen(port, () => {
+    console.log(`🚀 Server is Running in port ${port}`);
+});
