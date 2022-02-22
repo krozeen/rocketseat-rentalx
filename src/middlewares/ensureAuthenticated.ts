e25468b5e2920f7cfb180b6aad0cb0fd8f32791e
Joao Freitas <joaovotk@gmail.com>
2022-02-21 23:28:31 -0300

    Criando middleware de autenticação por token, e busca de usuario por ID

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import { UserRepository } from '../modules/accounts/entities/repositories/implementations/UserRepository';
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new Error('Token Missing from request');
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { sub: userId } = verify(
+      token,
+      'f00a690212b11159d6c741822e49ac5c',
+    ) as IPayload;
+
+    const usersRepository = new UserRepository();
+
+    const userAlreadyExists = await usersRepository.findById(userId);
+
+    if (!userAlreadyExists) {
+      throw new Error('User not found');
+    }
+
+    next();
+  } catch (error) {
+    throw new Error('Invalid Token');
+  }
+}
--- src/modules/accounts/entities/repositories/IUserRepository.ts
+++ src/modules/accounts/entities/repositories/IUserRepository.ts
@@ -2,10 +2,10 @@ import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
 import { User } from '../User';
 
 interface IUserRepository {
-  [x: string]: any;
   create(data: ICreateUserDTO): Promise<void>;
   // list(): Promise<User[]>;
   findByEmail(name: string): Promise<User>;
+  findById(id: string): Promise<User>;
 }
 
 export { IUserRepository };
